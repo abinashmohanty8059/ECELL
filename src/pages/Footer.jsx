d9ec637aa771319dd3eeff143f114df0fa4b8bbe
rupanjanas <saharupanjana04@gmail.com>
2025-06-24 10:12:45 +0530

    Update Footer.jsx

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -88,13 +88,15 @@ function Footer() {
         </div>
       </div>
       <div className="flex justify-end">
-      <Link
-      to="/contact"
-      className="text-white border border-blue-500 rounded-2xl px-6 py-2 hover:bg-blue-500 hover:text-black transition duration-300 "
-    >
-      Contact Us
-    </Link>
+  <Link
+    to="/contact"
+    onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+    className="text-white border border-blue-500 rounded-2xl px-6 py-2 hover:bg-blue-500 hover:text-black transition duration-300"
+  >
+    Contact Us
+  </Link>
 </div>
+
       {/* Social Networks Section */}
       <div className="max-w-7xl mx-auto mt-8 pt-8 border-t border-gray-700 flex flex-col sm:flex-col ">
         <p className="text-sm mb-4 sm:mb-0 text-center sm:text-left">Get Connected with us on social networks:</p>
